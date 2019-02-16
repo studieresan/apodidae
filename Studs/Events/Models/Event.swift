0acb174f1d581d3d90c7694b8b91abaad7610239
Willy Liu <arkazain@gmail.com>
2019-02-16 20:46:15 +0100

    Add function to fetch events, update Event model

--- Studs/Events/Models/Event.swift
+++ Studs/Events/Models/Event.swift
@@ -8,15 +8,24 @@
 
 import Foundation
 
+struct AllEventsResponse: Decodable {
+    let data: AllEvents
+}
+
+struct AllEvents: Decodable {
+    let allEvents: [Event]
+}
+
 struct Event: Decodable {
     let id: String
     let companyName: String?
     let schedule: String?
     let location: String?
     let privateDescription: String?
+    let publicDescription: String?
     let beforeSurveys: [String]?
     let afterSurveys: [String]?
-    let date: Date?
+    let date: String?
 
     enum EventCodingKeys: String, CodingKey {
         case id
@@ -24,6 +33,7 @@ struct Event: Decodable {
         case schedule
         case location
         case privateDescription
+        case publicDescription
         case beforeSurveys
         case afterSurveys
         case date
@@ -32,7 +42,15 @@ struct Event: Decodable {
 
 extension Event: Comparable {
     static func < (lhs: Event, rhs: Event) -> Bool {
-        return lhs.date ?? Date.distantPast < rhs.date ?? Date.distantPast
+        let dateFormatter = ISO8601DateFormatter()
+        // iOS <11 can't parse ISO8601 dates with milliseconds, so we remove them
+        let trimmedLhsDate = lhs.date!.replacingOccurrences(of: "\\.\\d+", with: "", options: .regularExpression)
+        let trimmedRhsDate = rhs.date!.replacingOccurrences(of: "\\.\\d+", with: "", options: .regularExpression)
+
+        let lhsDate = dateFormatter.date(from: trimmedLhsDate)
+        let rhsDate = dateFormatter.date(from: trimmedRhsDate)
+
+        return lhsDate ?? Date.distantPast < rhsDate ?? Date.distantPast
     }
 
     static func == (lhs: Event, rhs: Event) -> Bool {
