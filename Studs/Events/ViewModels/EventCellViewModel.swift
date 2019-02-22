bf0ce3fd962cbced3d056ced680f9e6b3345cbd3
Willy Liu <arkazain@gmail.com>
2019-02-22 16:20:18 +0100

    Refactor events for MVVM pattern

---
+++ Studs/Events/ViewModels/EventCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  EventCellViewModel.swift
+//  Studs
+//
+//  Created by Willy Liu on 2019-02-22.
+//  Copyright © 2019 Studieresan. All rights reserved.
+//
+
+import Foundation
+
+struct EventCellViewModel {
+    let month: String
+    let day: String
+    let companyName: String?
+}
