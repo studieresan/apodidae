1bcf31ba2dba52bf67fe152e2fe2c8a7f5918030
Willy Liu <arkazain@gmail.com>
2019-03-07 18:55:30 +0100

    Add map to event detail screen

--- Studs/Events/Views/EventDetailViewController.swift
+++ Studs/Events/Views/EventDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 
 final class EventDetailViewController: UIViewController {
 
@@ -15,6 +16,7 @@ final class EventDetailViewController: UIViewController {
     public var event: Event?
     @IBOutlet weak var closeButton: UIButton!
     @IBOutlet weak var companyName: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
 
     // MARK: Lifecycle
 
@@ -26,16 +28,64 @@ final class EventDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        if let event = event {
-            companyName.text = event.companyName
+        guard let event = event else {
+            fatalError("Did not load event from previous screen")
         }
+
+        companyName.text = event.companyName
+        setInitialLocation()
     }
 
     // MARK: Actions
 
     @IBAction func onClose(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+
+    // MARK: Private methods
+
+    private func setInitialLocation() {
+        guard let address = event?.location else {
+            fatalError("Event location isn't set")
+        }
+
+        getCoordinate(addressString: address) { location, error in
+            if error != nil {
+                return
+            }
+
+            let regionRadius: CLLocationDistance = 500
+    
+            let annotation = MKPointAnnotation().apply {
+                $0.coordinate = location
+                $0.title = address
+            }
+
+            let coordinateRegion = MKCoordinateRegion(center: location,
+                                                    latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+            self.mapView.run {
+                $0.addAnnotation(annotation)
+                $0.setRegion(coordinateRegion, animated: true)
+            }
+        }
+    }
+
+    // Converts an address to coordinates.
+    // Source: https://developer.apple.com/documentation/corelocation/converting_between_coordinates_and_user-friendly_place_names
+    func getCoordinate(addressString: String, completionHandler: @escaping(CLLocationCoordinate2D, NSError?) -> Void ) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(addressString) { (placemarks, error) in
+            if error == nil {
+                if let placemark = placemarks?[0] {
+                    let location = placemark.location!
+
+                    completionHandler(location.coordinate, nil)
+                    return
+                }
+            }
+
+            completionHandler(kCLLocationCoordinate2DInvalid, error as NSError?)
+        }
     }
 
 }
