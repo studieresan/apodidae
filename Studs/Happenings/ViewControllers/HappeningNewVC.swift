8f489712e29cc3471e0d036becdfb4238f78f716
Glenn Olsson <glennholsson@gmail.com>
2021-04-07 11:48:10 +0200

    Embed new happning view in scroll view

--- Studs/Happenings/ViewControllers/HappeningNewVC.swift
+++ Studs/Happenings/ViewControllers/HappeningNewVC.swift
@@ -12,8 +12,21 @@ import UIKit
 class HappeningNewViewController: UIViewController {
 
 	var pickedEmoji: String?
+	var location: GeoJSON?
+	var companions: [User] = []
 
 	@IBOutlet var emojiButtons: [UIButton]!
+
+	@IBOutlet var descriptionField: UITextView!
+
+	@IBAction func saveButtonPressed(_ sender: Any) {
+		print("SAVE")
+	}
+
+	@objc func hideKeyboard() {
+		print("HIDE")
+		self.view.endEditing(true)
+	}
 
 	@IBAction func emojiButtonPressed(_ sender: Any) {
 		for emojiButton in self.emojiButtons {
@@ -30,6 +43,30 @@ class HappeningNewViewController: UIViewController {
 	}
 
 	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		//Set bar title font
 		self.navigationController?.navigationBar.titleTextAttributes = [.font: UIFont.preferredFont(forTextStyle: .title1)]
+
+		self.descriptionField.delegate = self
+		self.descriptionField.returnKeyType = .done
+
+	}
+
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		self.hideKeyboard()
+	}
+}
+
+extension HappeningNewViewController: UITextViewDelegate {
+
+	func textAreaShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+
+		print("Should return ")
+		self.hideKeyboard()
+		textField.endEditing(true)
+
+		return true
 	}
 }
